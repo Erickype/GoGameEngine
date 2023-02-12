585c4fad55539b2c3d2dffba9b710a2b6a66d6f1
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-11 20:33:36 -0500

    GetInstance implementing singleton of the Manager struct

--- Render/RenderManager.go
+++ Render/RenderManager.go
@@ -3,7 +3,14 @@ package Render
 type Manager struct {
 }
 
-func (m *Manager) GetInstance() {}
+var instance *Manager
+
+func (m *Manager) GetInstance() *Manager {
+	if instance == nil {
+		instance = &Manager{}
+	}
+	return instance
+}
 
 func (m *Manager) Start() {}
 
