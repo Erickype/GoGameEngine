4569b174395f873423d82aec1be3816f23229a9b
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-16 01:00:17 -0500

    LogSystem initialization

--- Core/core.go
+++ Core/core.go
@@ -1,1 +1,14 @@
 package Core
+
+import "github.com/Erickype/GoGameEngine/Log"
+
+func InitLogSystem() (*Log.CoreLogger, *Log.ClientLogger) {
+
+	coreLogger := &Log.CoreLogger{}
+	coreLogger.Init()
+
+	clientLogger := &Log.ClientLogger{}
+	clientLogger.Init()
+
+	return coreLogger, clientLogger
+}
