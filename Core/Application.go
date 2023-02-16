1a93235fba2f4c557f1955e8a9e55f8b9ff8e466
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-16 14:39:53 -0500

    Test of the logger using all methods

--- Core/Application.go
+++ Core/Application.go
@@ -17,18 +17,23 @@ type Application struct {
 }
 
 func (a *Application) run() {
-	for {
+	for i := 0; i < 10; i++ {
 		time.Sleep(time.Second)
 		a.clientLogger.Info("Running")
 	}
 }
 
 func (a *Application) destroy() {
+	a.coreLogger.Fatal("Destroying: ", true)
 }
 
 func (a *Application) init() {
 	a.coreLogger, a.clientLogger = InitLogSystem()
 	a.coreLogger.Info("Starting engine!!")
+	a.coreLogger.Warn("Develop version: ", true)
+	a.coreLogger.Error("Errors: ", false)
+	a.coreLogger.Debug("Debug: ", true)
+	a.coreLogger.Trace("Trace")
 }
 
 // CreateApplication This is the entry point to create an application
