8764b6cdcd25ad67432ee65d273555fce0c47a59
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-18 17:29:11 -0500

    MouseMovedEvent that inherits Event and implements his own methods

--- Events/mouse.go
+++ Events/mouse.go
@@ -1,1 +1,26 @@
 package Events
+
+type IMouseMovedEvent interface {
+	GetX() float64
+	GetY() float64
+	Init()
+}
+
+type MouseMovedEvent struct {
+	*Event
+	mouseX float64
+	mouseY float64
+}
+
+func (m *MouseMovedEvent) GetX() float64 {
+	return m.mouseX
+}
+
+func (m *MouseMovedEvent) GetY() float64 {
+	return m.mouseY
+}
+
+func (m *MouseMovedEvent) Init() {
+	m.Event.eventType = MouseMoved
+	m.Event.eventCategory = Mouse | Input
+}
