d7de5d46803bc0fc4da3e7e259474d7beda4bc4e
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-17 23:12:48 -0500

    Event Types enum and his String function

--- Event/event.go
+++ Event/event.go
@@ -1,1 +1,58 @@
 package Event
+
+type Types int
+
+const (
+	None Types = iota
+	WindowClose
+	WindowResize
+	WindowFocus
+	WindowLostFocus
+	WindowMoved
+	AppTick
+	AppUpdate
+	AppRender
+	KeyPressed
+	KeyReleased
+	MouseButtonPressed
+	MouseButtonReleased
+	MouseMoved
+	MouseScrolled
+)
+
+func (e Types) String() string {
+	switch e {
+	case None:
+		return "None"
+	case WindowClose:
+		return "WindowClose"
+	case WindowResize:
+		return "WindowResize"
+	case WindowFocus:
+		return "WindowFocus"
+	case WindowLostFocus:
+		return "WindowLostFocus"
+	case WindowMoved:
+		return "WindowMoved"
+	case AppTick:
+		return "AppTick"
+	case AppUpdate:
+		return "AppUpdate"
+	case AppRender:
+		return "AppRender"
+	case KeyPressed:
+		return "KeyPressed"
+	case KeyReleased:
+		return "KeyReleased"
+	case MouseButtonPressed:
+		return "MouseButtonPressed"
+	case MouseButtonReleased:
+		return "MouseButtonReleased"
+	case MouseMoved:
+		return "MouseMoved"
+	case MouseScrolled:
+		return "MouseScrolled"
+	default:
+		return "Unknown"
+	}
+}
