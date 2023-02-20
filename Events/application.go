c5568da306dd5a757ef946ece5532d712e3247b4
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-20 14:58:53 -0500

    WindowResizeEvent implemented

--- Events/application.go
+++ Events/application.go
@@ -1,1 +1,32 @@
 package Events
+
+import "fmt"
+
+// IWindowResizeEvent interface to implement WindowResizeEvent
+type IWindowResizeEvent interface {
+	GetWidth() int
+	GetHeight() int
+}
+
+type WindowResizeEvent struct {
+	*Event
+	width  int
+	height int
+}
+
+func (w *WindowResizeEvent) GetWidth() int {
+	return w.width
+}
+
+func (w *WindowResizeEvent) GetHeight() int {
+	return w.height
+}
+
+func (w *WindowResizeEvent) ToString() string {
+	return fmt.Sprintf("WindowResizeEvent: %d, %d", w.width, w.height)
+}
+
+func (w *WindowResizeEvent) Init() {
+	w.eventType = WindowResize
+	w.eventCategory = Application
+}
