cc4c94cdb6cea22e8f55a9a0ab6476b9640350d6
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-21 14:10:19 -0500

    Dispatch function that call the specific event function, implemented for MouseMovedEvent

--- Events/eventDispatcher.go
+++ Events/eventDispatcher.go
@@ -8,11 +8,22 @@ func init() {
 
 type IEventDispatcher interface {
 	GetInstance() *EventDispatcher
+	Dispatch(event IEvent) bool
 }
 
-type EventDispatcher struct {
-}
+type EventDispatcher struct{}
 
 func (d *EventDispatcher) GetInstance() *EventDispatcher {
 	return instance
+}
+
+func (d *EventDispatcher) Dispatch(event IEvent) bool {
+	response := false
+	switch event.(type) {
+	case *MouseMovedEvent:
+		mouseMovedEvent := event.(*MouseMovedEvent)
+		mouseMovedEvent.handled = true
+		response = mouseMovedEvent.handled
+	}
+	return response
 }
