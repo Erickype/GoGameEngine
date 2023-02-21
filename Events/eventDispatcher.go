c31992505fac827630415058f3a9e6a32838aeb0
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-21 15:02:34 -0500

    Type assertion not necessary in type switches

--- Events/eventDispatcher.go
+++ Events/eventDispatcher.go
@@ -19,39 +19,31 @@ func (d *EventDispatcher) GetInstance() *EventDispatcher {
 
 func (d *EventDispatcher) Dispatch(event IEvent) bool {
 	response := false
-	switch event.(type) {
+	switch event := event.(type) {
 	case *MouseMovedEvent:
-		concreteEvent := event.(*MouseMovedEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	case *MouseScrolledEvent:
-		concreteEvent := event.(*MouseScrolledEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	case *MouseButtonPressedEvent:
-		concreteEvent := event.(*MouseButtonPressedEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	case *MouseButtonReleaseEvent:
-		concreteEvent := event.(*MouseButtonReleaseEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	case *KeyPressedEvent:
-		concreteEvent := event.(*KeyPressedEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	case *KeyReleasedEvent:
-		concreteEvent := event.(*KeyReleasedEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	case *WindowResizeEvent:
-		concreteEvent := event.(*WindowResizeEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	case *WindowCloseEvent:
-		concreteEvent := event.(*WindowCloseEvent)
-		concreteEvent.handled = true
-		response = concreteEvent.handled
+		event.handled = true
+		response = event.handled
 	}
 	return response
 }
