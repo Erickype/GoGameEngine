54a77e891fa570b2a169912c8905da44cbcacf99
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-21 15:03:08 -0500

    Manager know creates any concrete event

--- Events/manager.go
+++ Events/manager.go
@@ -2,7 +2,7 @@ package Events
 
 type IEventManager interface {
 	GetDispatcher() *EventDispatcher
-	CreateEvent(eventType Type) *MouseMovedEvent
+	CreateEvent(eventType EventType) *IEvent
 }
 
 type EventManager struct {
@@ -13,23 +13,22 @@ func (e *EventManager) GetDispatcher() *EventDispatcher {
 	return e.eventDispatcher.GetInstance()
 }
 
-func (e *EventManager) CreateEvent(eventType Type) *MouseMovedEvent {
+func (e *EventManager) CreateEvent(eventType EventType) IEvent {
 
 	event := Event{
-		dispatcher:    e.GetDispatcher(),
 		handled:       false,
 		eventCategory: 0,
 		eventType:     0,
 	}
-	var newEvent MouseMovedEvent
 
 	switch eventType {
 	case MouseMoved:
-		newEvent = MouseMovedEvent{
+		return &MouseMovedEvent{
 			Event:  &event,
 			mouseX: 0,
 			mouseY: 0,
 		}
+	default:
+		return nil
 	}
-	return &newEvent
 }
