0c8cb3c1930cc41bb579460e545bb81c3435132d
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-23 18:00:37 -0500

    Window abstraction, properties, callback function and IWindow interface

--- Window/window.go
+++ Window/window.go
@@ -1,1 +1,39 @@
 package Window
+
+import "github.com/Erickype/GoGameEngine/Events"
+
+type EventCallBackFn func(event *Events.IEvent)
+
+type Properties struct {
+	title  string
+	width  int
+	height int
+}
+
+var windowProps *Properties
+
+func init() {
+	windowProps = &Properties{
+		title:  "GoGameEngine",
+		width:  1280,
+		height: 720,
+	}
+}
+
+// SetWindowProps will be used to set new title, width, height
+func SetWindowProps(title string, width int, height int) {
+	windowProps.title = title
+	windowProps.width = width
+	windowProps.height = height
+}
+
+type IWindow interface {
+	GetWidth() int
+	GetHeight() int
+	SetEventCallback(callback *EventCallBackFn)
+	SetVsync(enabled bool)
+	IsVSync() bool
+	OnUpdate()
+	Create(props *Properties) IWindow
+	Init()
+}
