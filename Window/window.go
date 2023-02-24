c6bf135c7ce14abbe437e01e4cb2ce10cb2d69ec
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-23 19:14:12 -0500

    Won't use that init function, Shutdown method added

--- Window/window.go
+++ Window/window.go
@@ -5,26 +5,9 @@ import "github.com/Erickype/GoGameEngine/Events"
 type EventCallBackFn func(event *Events.IEvent)
 
 type Properties struct {
-	title  string
-	width  int
-	height int
-}
-
-var windowProps *Properties
-
-func init() {
-	windowProps = &Properties{
-		title:  "GoGameEngine",
-		width:  1280,
-		height: 720,
-	}
-}
-
-// SetWindowProps will be used to set new title, width, height
-func SetWindowProps(title string, width int, height int) {
-	windowProps.title = title
-	windowProps.width = width
-	windowProps.height = height
+	Title  string
+	Width  int
+	Height int
 }
 
 type IWindow interface {
@@ -34,6 +17,6 @@ type IWindow interface {
 	SetVsync(enabled bool)
 	IsVSync() bool
 	OnUpdate()
-	Create(props *Properties) IWindow
+	Shutdown()
 	Init()
 }
