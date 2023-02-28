5211669e80dbedf9d871b0b5ba6a0a85472c67a6
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-28 11:09:42 -0500

    SetSizeCallback for window, use of the callback function in Window package

--- Platform/Windows/window.go
+++ Platform/Windows/window.go
@@ -2,6 +2,7 @@ package Windows
 
 import (
 	common "github.com/Erickype/GoGameEngine/Common"
+	"github.com/Erickype/GoGameEngine/Events"
 	abstractWindow "github.com/Erickype/GoGameEngine/Window"
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"unsafe"
@@ -71,6 +72,25 @@ func (w *Window) Init() {
 	w.glfwWindow = window
 	w.glfwWindow.MakeContextCurrent()
 	w.glfwWindow.SetUserPointer(unsafe.Pointer(w.data))
+
+	w.glfwWindow.SetSizeCallback(func(window *glfw.Window, width int, height int) {
+		data := (*data)(window.GetUserPointer())
+		data.width = width
+		data.height = height
+
+		event := common.EventsFactory.CreateEvent(Events.WindowResize)
+
+		// Type assertion to retrieve the WindowResizeEvent value from the IEvent interface type
+		if resizeEvent, ok := event.(*Events.WindowResizeEvent); ok {
+			// Use the resizeEvent variable of type *Events.WindowResizeEvent to access its properties
+			resizeEvent.Width = width
+			resizeEvent.Height = height
+		}
+
+		if data.eventCallback != nil {
+			(*data.eventCallback)(&event)
+		}
+	})
 }
 
 func initGlfw() {
