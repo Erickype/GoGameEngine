818a6ef4d4d6863b0ac27bb5b487986d8a91480d
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-02 11:49:20 -0500

    Layer struct that holds the debugName of the layer

--- Core/layer.go
+++ Core/layer.go
@@ -10,3 +10,22 @@ type iLayer interface {
 	GetName() string
 	Construct(debugName string)
 }
+type Layer struct {
+	debugName string
+}
+
+func (l *Layer) OnAttach() {}
+
+func (l *Layer) OnDetach() {}
+
+func (l *Layer) OnUpdate() {}
+
+func (l *Layer) OnEvent(_ *Events.IEvent) {}
+
+func (l *Layer) GetName() string {
+	return l.debugName
+}
+
+func (l *Layer) Construct(debugName string) {
+	l.debugName = debugName
+}
