778b649e993c9dcd2475afd4ac815c440340504e
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-11 14:04:13 -0500

    Abstract interface for Renderer

--- API/Platform/iPlatform.go
+++ API/Platform/iPlatform.go
@@ -1,5 +1,7 @@
 package Platform
 
+import "github.com/AllenDang/cimgui-go"
+
 // IPlatform covers mouse/keyboard/gamepad inputs, cursor shape, timing, windowing.
 type IPlatform interface {
 	// ShouldStop is regularly called as the abort condition for the program loop.
@@ -10,12 +12,20 @@ type IPlatform interface {
 	DisplaySize() [2]float32
 	// FramebufferSize returns the dimension of the framebuffer.
 	FramebufferSize() [2]float32
-	// NewFrame marks the begin of a render pass. It must update the imgui IO state according to user input (mouse, keyboard, ...)
+	// NewFrame marks the beginning of a render pass. It must update the imGui IO state according to user input (mouse, keyboard, ...)
 	NewFrame()
-	// PostRender marks the completion of one render pass. Typically this causes the display buffer to be swapped.
+	// PostRender marks the completion of one render pass. Typically, this causes the display buffer to be swapped.
 	PostRender()
 	// ClipboardText returns the current text of the clipboard, if available.
 	ClipboardText() (string, error)
 	// SetClipboardText sets the text as the current text of the clipboard.
 	SetClipboardText(text string)
+}
+
+// IRenderer covers rendering imGui draw data.
+type IRenderer interface {
+	// PreRender causes the display buffer to be prepared for new output.
+	PreRender(clearColor [3]float32)
+	// Render draws the provided imGui draw data.
+	Render(displaySize [2]float32, framebufferSize [2]float32, drawData imgui.DrawData)
 }
