bb8bf889f9c9967d396d2a60d3f689bb5cd57b52
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-11 13:22:06 -0500

    Render function

--- API/Internal/renderers/OpenGL3.go
+++ API/Internal/renderers/OpenGL3.go
@@ -1,10 +1,9 @@
 package renderers
 
 import (
-	_ "embed" // using embed for the shader sources
+	_ "embed"
 	"fmt"
 	"github.com/AllenDang/cimgui-go"
-
 	"github.com/Erickype/GoGameEngine/API/Internal/renderers/gl/v3.2-core/gl"
 )
 
@@ -14,7 +13,6 @@ var unVersionedVertexShader string
 //go:embed gl-shader/main.frag
 var unVersionedFragmentShader string
 
-// OpenGL3 implements a renderer based on github.com/go-gl/gl (v3.2-core).
 type OpenGL3 struct {
 	imGuiIO imgui.IO
 
@@ -32,8 +30,6 @@ type OpenGL3 struct {
 	elementsHandle         uint32
 }
 
-// NewOpenGL3 attempts to initialize a renderer.
-// An OpenGL context has to be established before calling this function.
 func NewOpenGL3(io imgui.IO) (*OpenGL3, error) {
 	err := gl.Init()
 	if err != nil {
@@ -56,7 +52,161 @@ func (renderer *OpenGL3) PreRender(clearColor [3]float32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 }
 
-// Dispose cleans up the resources.
+func (renderer *OpenGL3) Render(displaySize [2]float32, framebufferSize [2]float32, drawData imgui.DrawData) {
+	// Avoid rendering when minimized, scale coordinates for retina displays (screen coordinates != framebuffer coordinates)
+	displayWidth, displayHeight := displaySize[0], displaySize[1]
+	fbWidth, fbHeight := framebufferSize[0], framebufferSize[1]
+	if (fbWidth <= 0) || (fbHeight <= 0) {
+		return
+	}
+	drawData.ScaleClipRects(imgui.Vec2{
+		X: fbWidth / displayWidth,
+		Y: fbHeight / displayHeight,
+	})
+
+	// Backup GL state
+	var lastActiveTexture int32
+	gl.GetIntegerv(gl.ACTIVE_TEXTURE, &lastActiveTexture)
+	gl.ActiveTexture(gl.TEXTURE0)
+	var lastProgram int32
+	gl.GetIntegerv(gl.CURRENT_PROGRAM, &lastProgram)
+	var lastTexture int32
+	gl.GetIntegerv(gl.TEXTURE_BINDING_2D, &lastTexture)
+	var lastSampler int32
+	gl.GetIntegerv(gl.SAMPLER_BINDING, &lastSampler)
+	var lastArrayBuffer int32
+	gl.GetIntegerv(gl.ARRAY_BUFFER_BINDING, &lastArrayBuffer)
+	var lastElementArrayBuffer int32
+	gl.GetIntegerv(gl.ELEMENT_ARRAY_BUFFER_BINDING, &lastElementArrayBuffer)
+	var lastVertexArray int32
+	gl.GetIntegerv(gl.VERTEX_ARRAY_BINDING, &lastVertexArray)
+	var lastPolygonMode [2]int32
+	gl.GetIntegerv(gl.POLYGON_MODE, &lastPolygonMode[0])
+	var lastViewport [4]int32
+	gl.GetIntegerv(gl.VIEWPORT, &lastViewport[0])
+	var lastScissorBox [4]int32
+	gl.GetIntegerv(gl.SCISSOR_BOX, &lastScissorBox[0])
+	var lastBlendSrcRgb int32
+	gl.GetIntegerv(gl.BLEND_SRC_RGB, &lastBlendSrcRgb)
+	var lastBlendDstRgb int32
+	gl.GetIntegerv(gl.BLEND_DST_RGB, &lastBlendDstRgb)
+	var lastBlendSrcAlpha int32
+	gl.GetIntegerv(gl.BLEND_SRC_ALPHA, &lastBlendSrcAlpha)
+	var lastBlendDstAlpha int32
+	gl.GetIntegerv(gl.BLEND_DST_ALPHA, &lastBlendDstAlpha)
+	var lastBlendEquationRgb int32
+	gl.GetIntegerv(gl.BLEND_EQUATION_RGB, &lastBlendEquationRgb)
+	var lastBlendEquationAlpha int32
+	gl.GetIntegerv(gl.BLEND_EQUATION_ALPHA, &lastBlendEquationAlpha)
+	lastEnableBlend := gl.IsEnabled(gl.BLEND)
+	lastEnableCullFace := gl.IsEnabled(gl.CULL_FACE)
+	lastEnableDepthTest := gl.IsEnabled(gl.DEPTH_TEST)
+	lastEnableScissorTest := gl.IsEnabled(gl.SCISSOR_TEST)
+
+	// Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, polygon fill
+	gl.Enable(gl.BLEND)
+	gl.BlendEquation(gl.FUNC_ADD)
+	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+	gl.Disable(gl.CULL_FACE)
+	gl.Disable(gl.DEPTH_TEST)
+	gl.Enable(gl.SCISSOR_TEST)
+	gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL)
+
+	// Setup viewport, orthographic projection matrix
+	// Our visible imGui space lies from draw_data->DisplayPos (top left) to draw_data->DisplayPos+data_data->DisplaySize (bottom right).
+	// DisplayMin is typically (0,0) for single viewport apps.
+	gl.Viewport(0, 0, int32(fbWidth), int32(fbHeight))
+	orthographicProjection := [4][4]float32{
+		{2.0 / displayWidth, 0.0, 0.0, 0.0},
+		{0.0, 2.0 / -displayHeight, 0.0, 0.0},
+		{0.0, 0.0, -1.0, 0.0},
+		{-1.0, 1.0, 0.0, 1.0},
+	}
+	gl.UseProgram(renderer.shaderHandle)
+	gl.Uniform1i(renderer.attribLocationTex, 0)
+	gl.UniformMatrix4fv(renderer.attribLocationProjMtx, 1, false, &orthographicProjection[0][0])
+	gl.BindSampler(0, 0) // Rely on combined texture/sampler state.
+
+	// Recreate the VAO every time
+	// (This is to easily allow multiple GL contexts. VAO are not shared among GL contexts, and
+	// we don't track creation/deletion of windows so, we don't have an obvious key to use to cache them.)
+	var vaoHandle uint32
+	gl.GenVertexArrays(1, &vaoHandle)
+	gl.BindVertexArray(vaoHandle)
+	gl.BindBuffer(gl.ARRAY_BUFFER, renderer.vboHandle)
+	gl.EnableVertexAttribArray(uint32(renderer.attribLocationPosition))
+	gl.EnableVertexAttribArray(uint32(renderer.attribLocationUV))
+	gl.EnableVertexAttribArray(uint32(renderer.attribLocationColor))
+	vertexSize, vertexOffsetPos, vertexOffsetUv, vertexOffsetCol := imgui.VertexBufferLayout()
+	gl.VertexAttribPointerWithOffset(uint32(renderer.attribLocationPosition), 2, gl.FLOAT, false, int32(vertexSize), uintptr(vertexOffsetPos))
+	gl.VertexAttribPointerWithOffset(uint32(renderer.attribLocationUV), 2, gl.FLOAT, false, int32(vertexSize), uintptr(vertexOffsetUv))
+	gl.VertexAttribPointerWithOffset(uint32(renderer.attribLocationColor), 4, gl.UNSIGNED_BYTE, true, int32(vertexSize), uintptr(vertexOffsetCol))
+	indexSize := imgui.IndexBufferLayout()
+	drawType := gl.UNSIGNED_SHORT
+	const bytesPerUint32 = 4
+	if indexSize == bytesPerUint32 {
+		drawType = gl.UNSIGNED_INT
+	}
+
+	// Draw
+	for _, list := range drawData.CommandLists() {
+		vertexBuffer, vertexBufferSize := list.GetVertexBuffer()
+		gl.BindBuffer(gl.ARRAY_BUFFER, renderer.vboHandle)
+		gl.BufferData(gl.ARRAY_BUFFER, vertexBufferSize, vertexBuffer, gl.STREAM_DRAW)
+
+		indexBuffer, indexBufferSize := list.GetIndexBuffer()
+		gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, renderer.elementsHandle)
+		gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, indexBufferSize, indexBuffer, gl.STREAM_DRAW)
+
+		for _, cmd := range list.Commands() {
+			if cmd.HasUserCallback() {
+				cmd.CallUserCallback(list)
+			} else {
+				gl.BindTexture(gl.TEXTURE_2D, uint32(uintptr(cmd.TextureId())))
+				clipRect := cmd.ClipRect()
+				gl.Scissor(int32(clipRect.X), int32(fbHeight)-int32(clipRect.W), int32(clipRect.Z-clipRect.X), int32(clipRect.W-clipRect.Y))
+				gl.DrawElementsBaseVertexWithOffset(gl.TRIANGLES, int32(cmd.ElemCount()), uint32(drawType),
+					uintptr(cmd.IdxOffset()), int32(cmd.VtxOffset()))
+			}
+		}
+	}
+	gl.DeleteVertexArrays(1, &vaoHandle)
+
+	// Restore modified GL state
+	gl.UseProgram(uint32(lastProgram))
+	gl.BindTexture(gl.TEXTURE_2D, uint32(lastTexture))
+	gl.BindSampler(0, uint32(lastSampler))
+	gl.ActiveTexture(uint32(lastActiveTexture))
+	gl.BindVertexArray(uint32(lastVertexArray))
+	gl.BindBuffer(gl.ARRAY_BUFFER, uint32(lastArrayBuffer))
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, uint32(lastElementArrayBuffer))
+	gl.BlendEquationSeparate(uint32(lastBlendEquationRgb), uint32(lastBlendEquationAlpha))
+	gl.BlendFuncSeparate(uint32(lastBlendSrcRgb), uint32(lastBlendDstRgb), uint32(lastBlendSrcAlpha), uint32(lastBlendDstAlpha))
+	if lastEnableBlend {
+		gl.Enable(gl.BLEND)
+	} else {
+		gl.Disable(gl.BLEND)
+	}
+	if lastEnableCullFace {
+		gl.Enable(gl.CULL_FACE)
+	} else {
+		gl.Disable(gl.CULL_FACE)
+	}
+	if lastEnableDepthTest {
+		gl.Enable(gl.DEPTH_TEST)
+	} else {
+		gl.Disable(gl.DEPTH_TEST)
+	}
+	if lastEnableScissorTest {
+		gl.Enable(gl.SCISSOR_TEST)
+	} else {
+		gl.Disable(gl.SCISSOR_TEST)
+	}
+	gl.PolygonMode(gl.FRONT_AND_BACK, uint32(lastPolygonMode[0]))
+	gl.Viewport(lastViewport[0], lastViewport[1], lastViewport[2], lastViewport[3])
+	gl.Scissor(lastScissorBox[0], lastScissorBox[1], lastScissorBox[2], lastScissorBox[3])
+}
+
 func (renderer *OpenGL3) Dispose() {
 	renderer.invalidateDeviceObjects()
 }
