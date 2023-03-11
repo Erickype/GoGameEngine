ceb34ec3f00b665af3fde608090707aeda778d26
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-11 12:17:17 -0500

    Function createDeviceObjects, still needs renderer.createFontsTexture()

--- API/Internal/renderers/OpenGL3.go
+++ API/Internal/renderers/OpenGL3.go
@@ -44,7 +44,7 @@ func NewOpenGL3(io imgui.IO) (*OpenGL3, error) {
 		imGuiIO:     io,
 		glslVersion: "#version 150",
 	}
-	//renderer.createDeviceObjects()
+	renderer.createDeviceObjects()
 
 	io.SetBackendFlags(io.BackendFlags() | imgui.BackendFlagsRendererHasVtxOffset)
 
@@ -54,4 +54,53 @@ func NewOpenGL3(io imgui.IO) (*OpenGL3, error) {
 // Dispose cleans up the resources.
 func (renderer *OpenGL3) Dispose() {
 	//renderer.invalidateDeviceObjects()
+}
+
+func (renderer *OpenGL3) createDeviceObjects() {
+	// Backup GL state
+	var lastTexture int32
+	var lastArrayBuffer int32
+	var lastVertexArray int32
+	gl.GetIntegerv(gl.TEXTURE_BINDING_2D, &lastTexture)
+	gl.GetIntegerv(gl.ARRAY_BUFFER_BINDING, &lastArrayBuffer)
+	gl.GetIntegerv(gl.VERTEX_ARRAY_BINDING, &lastVertexArray)
+
+	vertexShader := renderer.glslVersion + "\n" + unVersionedVertexShader
+
+	fragmentShader := renderer.glslVersion + "\n" + unVersionedFragmentShader
+
+	renderer.shaderHandle = gl.CreateProgram()
+	renderer.vertHandle = gl.CreateShader(gl.VERTEX_SHADER)
+	renderer.fragHandle = gl.CreateShader(gl.FRAGMENT_SHADER)
+
+	glShaderSource := func(handle uint32, source string) {
+		csource, free := gl.Strs(source + "\x00")
+		defer free()
+
+		gl.ShaderSource(handle, 1, csource, nil)
+	}
+
+	glShaderSource(renderer.vertHandle, vertexShader)
+	glShaderSource(renderer.fragHandle, fragmentShader)
+	gl.CompileShader(renderer.vertHandle)
+	gl.CompileShader(renderer.fragHandle)
+	gl.AttachShader(renderer.shaderHandle, renderer.vertHandle)
+	gl.AttachShader(renderer.shaderHandle, renderer.fragHandle)
+	gl.LinkProgram(renderer.shaderHandle)
+
+	renderer.attribLocationTex = gl.GetUniformLocation(renderer.shaderHandle, gl.Str("Texture"+"\x00"))
+	renderer.attribLocationProjMtx = gl.GetUniformLocation(renderer.shaderHandle, gl.Str("ProjMtx"+"\x00"))
+	renderer.attribLocationPosition = gl.GetAttribLocation(renderer.shaderHandle, gl.Str("Position"+"\x00"))
+	renderer.attribLocationUV = gl.GetAttribLocation(renderer.shaderHandle, gl.Str("UV"+"\x00"))
+	renderer.attribLocationColor = gl.GetAttribLocation(renderer.shaderHandle, gl.Str("Color"+"\x00"))
+
+	gl.GenBuffers(1, &renderer.vboHandle)
+	gl.GenBuffers(1, &renderer.elementsHandle)
+
+	//renderer.createFontsTexture()
+
+	// Restore modified GL state
+	gl.BindTexture(gl.TEXTURE_2D, uint32(lastTexture))
+	gl.BindBuffer(gl.ARRAY_BUFFER, uint32(lastArrayBuffer))
+	gl.BindVertexArray(uint32(lastVertexArray))
 }
