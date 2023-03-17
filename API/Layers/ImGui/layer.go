81fc56b61d3b3c29345df4498ae301337f53445d
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-17 15:40:22 -0500

    OnMouseButtonPressedEvent and OnMouseMovedEvent to test dispatcher

--- API/Layers/ImGui/layer.go
+++ API/Layers/ImGui/layer.go
@@ -2,6 +2,7 @@ package Layers
 
 import (
 	"github.com/AllenDang/cimgui-go"
+	"github.com/Erickype/GoGameEngine/API/Common"
 	"github.com/Erickype/GoGameEngine/API/Events"
 	"github.com/Erickype/GoGameEngine/Core"
 	"unsafe"
@@ -31,11 +32,20 @@ func (l *Layer) OnUpdate() {
 	Core.ApplicationInstance.GetRenderer().Render(Core.ApplicationInstance.GetPlatform().DisplaySize(), Core.ApplicationInstance.GetPlatform().FramebufferSize(), imgui.CurrentDrawData())
 }
 
-func (l *Layer) OnEvent(_ *Events.IEvent) {
+func (l *Layer) OnEvent(event *Events.IEvent) {
+	dispatcher := Events.CreateDispatcher(event)
 
+	dispatcher.Dispatch(l.OnMouseButtonPressedEvent)
+	dispatcher.Dispatch(l.OnMouseMovedEvent)
 }
 
 func (l *Layer) OnMouseButtonPressedEvent(event *Events.MouseButtonPressedEvent) bool {
+	Common.CoreLogger.Debug("OnMouseButtonPressed: ", event)
+	return false
+}
+
+func (l *Layer) OnMouseMovedEvent(event *Events.MouseMovedEvent) bool {
+	Common.CoreLogger.Debug("OnMouseMoved: ", event)
 	return false
 }
 
