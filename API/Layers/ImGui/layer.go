1a7f4e8f73fceedb32f53fe01bfbfef51168db1d
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-17 15:53:13 -0500

    Declaration of the rest of the functions

--- API/Layers/ImGui/layer.go
+++ API/Layers/ImGui/layer.go
@@ -36,7 +36,12 @@ func (l *Layer) OnEvent(event *Events.IEvent) {
 	dispatcher := Events.CreateDispatcher(event)
 
 	dispatcher.Dispatch(l.OnMouseButtonPressedEvent)
+	dispatcher.Dispatch(l.OnMouseButtonReleasedEvent)
 	dispatcher.Dispatch(l.OnMouseMovedEvent)
+	dispatcher.Dispatch(l.OnMouseScrolledEvent)
+	dispatcher.Dispatch(l.OnKeyPressedEvent)
+	dispatcher.Dispatch(l.OnKeyReleasedEvent)
+	dispatcher.Dispatch(l.OnWindowResizeEvent)
 }
 
 func (l *Layer) OnMouseButtonPressedEvent(event *Events.MouseButtonPressedEvent) bool {
@@ -44,11 +49,34 @@ func (l *Layer) OnMouseButtonPressedEvent(event *Events.MouseButtonPressedEvent)
 	return false
 }
 
-func (l *Layer) OnMouseMovedEvent(event *Events.MouseMovedEvent) bool {
-	Common.CoreLogger.Debug("OnMouseMoved: ", event)
+func (l *Layer) OnMouseButtonReleasedEvent(event *Events.MouseButtonReleaseEvent) bool {
+	Common.CoreLogger.Debug("OnMouseButtonReleased: ", event)
 	return false
 }
 
+func (l *Layer) OnMouseMovedEvent(event *Events.MouseMovedEvent) bool {
+	Common.CoreLogger.Debug("OnMouseMovedEvent: ", event)
+	return false
+}
+
+func (l *Layer) OnMouseScrolledEvent(event *Events.MouseScrolledEvent) bool {
+	Common.CoreLogger.Debug("MouseScrolledEvent: ", event)
+	return false
+}
+
+func (l *Layer) OnKeyPressedEvent(event *Events.KeyPressedEvent) bool {
+	Common.CoreLogger.Debug("OnKeyPressedEvent: ", event)
+	return false
+}
+
+func (l *Layer) OnKeyReleasedEvent(event *Events.KeyReleasedEvent) bool {
+	Common.CoreLogger.Debug("OnKeyReleasedEvent: ", event)
+	return false
+}
+func (l *Layer) OnWindowResizeEvent(event *Events.WindowResizeEvent) bool {
+	Common.CoreLogger.Debug("OnWindowResizeEvent: ", event)
+	return false
+}
 func NewImGui() *Layer {
 	layer := Layer{}
 	layer.Construct("ImGui")
