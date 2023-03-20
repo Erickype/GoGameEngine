d895e69c97f1c49759ed28cdf505e963ca100087
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-20 15:21:57 -0500

    Application now uses the window interface to accept all platforms that implements that interface

--- Core/Application.go
+++ Core/Application.go
@@ -1,61 +1,59 @@
 package Core
 
 import (
+	"github.com/Erickype/GoGameEngine/API"
 	"github.com/Erickype/GoGameEngine/API/Common"
 	"github.com/Erickype/GoGameEngine/API/Events"
 	"github.com/Erickype/GoGameEngine/API/Internal"
-	"github.com/Erickype/GoGameEngine/API/Platform/Windows"
 	"github.com/Erickype/GoGameEngine/API/Window"
 )
 
 type IApplication interface {
 	run()
 	destroy()
-	init(layer *ILayer)
+	init(layer *ILayer, window *Window.IWindow)
 	onEvent(event *Events.IEvent)
 	PushLayer(layer *ILayer)
 	PushOverlay(overlay *ILayer)
-	GetPlatform(platform *Internal.IPlatform)
-	GetRenderer(platform *Internal.IRenderer)
+	GetPlatform() *Internal.IPlatform
+	GetRenderer() *Internal.IRenderer
 }
 
 var ApplicationInstance *Application
 
 type Application struct {
-	window     *Windows.Window
+	window     *Window.IWindow
 	running    bool
 	layerStack *LayerStack
 }
 
 func (a *Application) run() {
-	for !a.window.Platform.ShouldStop() {
+	for !(*(*a.window).GetPlatform()).ShouldStop() {
 		if *a.layerStack.layerInsert != 0 {
 			for _, layer := range *a.layerStack.layers {
 				(*layer).OnUpdate()
 			}
 		}
-		a.window.OnUpdate()
+		Common.CoreLogger.Fatal("Click: ", (*API.GetInputInstance()).IsKeyPressed(32))
+
+		(*a.window).OnUpdate()
 	}
 }
 
 func (a *Application) destroy() {
 	a.running = false
-	a.window.Shutdown()
+	(*a.window).Shutdown()
 }
 
-func (a *Application) init(layer *ILayer) {
+func (a *Application) init(layer *ILayer, window *Window.IWindow) {
 	Common.CoreLogger.Info("Starting engine!!")
 	a.running = true
-	a.window = Windows.Create(&Window.Properties{
-		Title:  "GoGameEngine",
-		Width:  800,
-		Height: 600,
-	})
+	a.window = window
 
 	eventCallbackFn := Window.EventCallBackFn(func(event *Events.IEvent) {
 		a.onEvent(event)
 	})
-	a.window.SetEventCallback(&eventCallbackFn)
+	(*a.window).SetEventCallback(&eventCallbackFn)
 	a.layerStack = &LayerStack{}
 	a.layerStack.Construct()
 	a.PushLayer(layer)
@@ -85,19 +83,19 @@ func (a *Application) PushOverlay(overlay *ILayer) {
 	a.layerStack.PushOverlay(overlay)
 }
 
-func (a *Application) GetPlatform() Internal.IPlatform {
-	return a.window.Platform
+func (a *Application) GetPlatform() *Internal.IPlatform {
+	return (*a.window).GetPlatform()
 }
 
-func (a *Application) GetRenderer() Internal.IRenderer {
-	return a.window.Renderer
+func (a *Application) GetRenderer() *Internal.IRenderer {
+	return (*a.window).GetRenderer()
 }
 
 // CreateApplication This is the entry point to create an application
-func CreateApplication(layer *ILayer) {
+func CreateApplication(layer *ILayer, window *Window.IWindow) {
 	application := &Application{}
 	ApplicationInstance = application
-	application.init(layer)
+	application.init(layer, window)
 	application.run()
 	application.destroy()
 }
